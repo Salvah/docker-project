e0fcabc4ddf26000f23babfa1bdae6b05cab4c28
= <tsalvah@gmail.com>
2022-12-20 10:46:38 -0300

    commit

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,34 @@
-const http = require("http");
-http
-  .createServer((req, res) => {
-    res.write("Hello World!");
-    res.end();
-  })
-  .listen(3000);
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const port = 3000;
+
+const { Sequelize, QueryTypes } = require("sequelize");
+
+const host = process.env.MYSQL_HOST || "docker";
+const user = process.env.MYSQL_USER || "root";
+const dbname = process.env.MYSQL_DATABASE || "docker";
+
+const sequelize = new Sequelize(dbname, user, "salvah", {
+  host,
+  dialect: "mysql",
+});
+
+app.get("/", async (req, res) => {
+  const users = await sequelize.query("SELECT * FROM `users`", {
+    type: QueryTypes.SELECT,
+  });
+  console.log(users);
+  res.send("Hello World!");
+});
+
+app.post("/", async (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.send("Teste");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
